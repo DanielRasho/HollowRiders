f56d016f1a9edced08458d67cc37ce50b1a7afbf
Daniel Alfredo Rayo Roldán <ray22933@uvg.edu.gt>
2026-04-19 17:23:15 -0600

    feat: basic setup

---
+++ Assets/Managers/TerrainManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour
+{
+    [SerializeField] private TextAsset dungeonFile;
+    const int MaxCells = 40;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
